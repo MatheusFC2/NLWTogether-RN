a5e31c79a15d4a90f8f5bb14ad0c67250ea3c4bd
MatheusFC2 <matheusfreitascampos@gmail.com>
2022-01-08 18:35:09 -0300

    docs: Create text on to profile and styles

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
-import { View } from 'react-native'
+import { View, Text } from 'react-native'
+import { styles } from './styles';
 
 export function Profile() {
     return (
-        <View>
+        <View style={styles.container}>
+            
+            <View >
+                <View style={styles.user}>
+                    <Text style={styles.greeting}>
+                        Olá,
+                    </Text>
 
+                    <Text style={styles.username}>
+                        Matheus
+                    </Text> 
+                    
+                </View>
+
+                <Text style={styles.message}>
+                    Hoje é dia de vitória
+                </Text>
+            </View>
         </View>    
     )
     
